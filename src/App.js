5521e273610767da8e2b1ffce1fc259d761cd3c8
GAKU <gakugaku1114@iCloud.com>
2023-10-11 02:54:48 +0000

    Update 3 files

--- src/App.js
+++ src/App.js
@@ -1,32 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.css";
 
 export const App = () => {
+  const [incompleteTodos, setIncompleteTodos] = useState(["aaaa", "bbbb"]);
   return (
     <>
-      <div>
+      <div className="input-area">
         <input placeholder="TODOを入力" />
         <button>追加</button>
       </div>
-      <div>
+      <div className="incomplete-area">
         <p>未完了のTODO</p>
         <ul>
-          <div>
+          <div className="list-row">
             <li>aaaa</li>
             <button>完了</button>
             <button>削除</button>
           </div>
-          <div>
-            <li>aaaa</li>
+          <div className="list-row">
+            <li>bbbb</li>
             <button>完了</button>
             <button>削除</button>
           </div>
         </ul>
       </div>
-      <div>
+      <div className="complete-area">
         <p>完了のTODO</p>
         <ul>
-          <div>
+          <div className="list-row">
             <li>aaaa</li>
             <button>戻す</button>
           </div>
