fe5fe3c13628b47b9a8644ce85316a78763e7e38
GAKU <gakugaku1114@iCloud.com>
2023-10-11 03:12:58 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,23 +5,22 @@ export const App = () => {
   const [incompleteTodos, setIncompleteTodos] = useState(["aaaa", "bbbb"]);
   return (
     <>
-      <div className="input-area">
+      <div key={todo} className="input-area">
         <input placeholder="TODOを入力" />
         <button>追加</button>
       </div>
       <div className="incomplete-area">
         <p>未完了のTODO</p>
         <ul>
-          <div className="list-row">
-            <li>aaaa</li>
-            <button>完了</button>
-            <button>削除</button>
-          </div>
-          <div className="list-row">
-            <li>bbbb</li>
-            <button>完了</button>
-            <button>削除</button>
-          </div>
+          {incompleteTodos.map((todo) => {
+            return (
+              <div className="list-row">
+                <li>{todo}</li>
+                <button>完了</button>
+                <button>削除</button>
+              </div>
+            );
+          })}
         </ul>
       </div>
       <div className="complete-area">
